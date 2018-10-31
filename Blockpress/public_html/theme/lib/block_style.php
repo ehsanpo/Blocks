88dbeaa8b606b13a0e4cae4be1fe367c72f31a0b
Ehsan Pourhadi <ehsan.po@gmail.com>
2018-10-31 13:40:54 +0100

    edited block styles css

--- Blockpress/public_html/theme/lib/block_style.php
+++ Blockpress/public_html/theme/lib/block_style.php
@@ -5,9 +5,9 @@
  */
 $layout_styles = function ($styles) {
 	$styles['Background color'] = array(
-		'bg_color_1' => 'Color 1',
-		'bg_color_2' => 'Color 2',
-		'bg_color_3' => 'Color 3'
+		'bg-color-1' => 'Color 1',
+		'bg-color-2' => 'Color 2',
+		'bg-color-3' => 'Color 3'
 	);
 
 	return $styles;
